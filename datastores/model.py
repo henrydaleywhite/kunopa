9b8489b5b0a9251d2e44daf6011b3f60a298b25b
henrydaleywhite <henry.daley.white@gmail.com>
2019-01-18 15:53:54 -0500

    add sql statements and mild refactoring

--- datastores/model.py
+++ datastores/model.py
@@ -3,6 +3,7 @@ from opencursor import OpenCursor
 
 
 class ParentIngredient:
+    """TODO docstring"""
     def __init__(self, row={}, name=''):
         if name:
             self._set_from_credentials(name)
@@ -22,19 +23,25 @@ class ParentIngredient:
 
     def _set_from_row(self, row):
         row = dict(row)
-        self.pk = row.get('pk')
-        self.name = row.get('name')
-        self.season = row.get('season')
-        self.taste = row.get('taste')
-        self.function = row.get('function')
-        self.botanical_relatives = row.get('botanical_relatives')
-        self.weight = row.get('weight')
-        self.volume = row.get('volume')
-        self.techniques = row.get('techniques')
-        self.tips = row.get('tips')
-        self.search_term = row.get('search_term')
+        for key, value in row.items():
+            setattr(self, key, value)
+        # self.pk = row.get('pk')        # self.name = row.get('name')        # self.season = row.get('season')        # self.taste = row.get('taste')        # self.function = row.get('function')        # self.botanical_relatives = row.get('botanical_relatives')        # self.weight = row.get('weight')        # self.volume = row.get('volume')        # self.techniques = row.get('techniques')        # self.tips = row.get('tips')        # self.search_term = row.get('search_term')
+
+    def get_children_of_parent(self):
+        """get the list of paired ingredients for the parent"""
+        with OpenCursor() as cur:
+            SQL = """ SELECT * FROM child_ingredients 
+                WHERE pairing_pk = ? ORDER BY pairing_strength; """
+            cur.execute(SQL, (self.pk,))
+            rows = cur.fetchall()
+        return [ChildIngredient(row) for row in rows]
+
+    def __bool__(self):
+        return bool(self.pk)
+
 
 class ChildIngredient:
+    """TODO docstring"""
     def __init__(self, row={}, name=''):
         if name:
             self._set_from_credentials(name)
@@ -43,7 +50,7 @@ class ChildIngredient:
 
     def _set_from_credentials(self, name):
         with OpenCursor() as cur:
-            SQL = """ SELECT * FROM parent_ingredients 
+            SQL = """ SELECT * FROM child_ingredients 
                 WHERE name = ?; """
             cur.execute(SQL, (name,))
             row = cur.fetchone()
@@ -54,22 +61,20 @@ class ChildIngredient:
 
     def _set_from_row(self, row):
         row = dict(row)
-        self.pk = row.get('pk')
-        self.name = row.get('name')
-        self.season = row.get('season')
-        self.taste = row.get('taste')
-        self.function = row.get('function')
-        self.botanical_relatives = row.get('botanical_relatives')
-        self.weight = row.get('weight')
-        self.volume = row.get('volume')
-        self.techniques = row.get('techniques')
-        self.tips = row.get('tips')
-        self.search_term = row.get('search_term')
+        for key, value in row.items():
+            setattr(self, key, value)
+        # self.pk = row.get('pk')        # self.name = row.get('name')        # self.pairing_strength = row.get('pairing_strength')        # self.search_term = row.get('search_term')        # self.pairing_pk = row.get('pairing_pk')        # self.own_parent_pk = row.get('own_parent_pk')
+        
+    def get_parent_for_child(self):
+        with OpenCursor() as cur:
+            SQL = """ SELECT * from parent_ingredients WHERE pk = ?; """
+            cur.execute(SQL, (self.own_parent_pk,))
+            row = cur.fetchone()
+        return ParentIngredient(row)
 
+    def __bool__(self):
+        return bool(self.pk)
 
-# TODO write select to pull children based on parent
-# SELECT * FROM child_ingredients WHERE pk = ?
-# cur.execute(SQL, (self.pk,))
-# TODO write select to pull parent based on selected child
-# SELECT * from parent_ingredients WHERE pk = ?
-# cur.execute(SQL, (self.own_parent_pk,))
+    def __repr__(self):
+        output = "Name: {}, Strength: {}"
+        return output.format(self.name, self.pairing_strength)
