b8b6c5b0b5350a21cd0771b329534d49a39de234
henrydaleywhite <henry.daley.white@gmail.com>
2019-01-22 15:39:38 -0500

    fix format ingred list function

--- spoonacular_api.py
+++ spoonacular_api.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 from pprint import pprint
@@ -17,14 +18,14 @@ def get_api_key(filename):
 
 def format_ingred_list(ing_list):
     """convert a list of ingredients into a string with
-    expected api format of '%2' between search terms
+    expected api format of '%2C' between search terms
     """
     return_str = ""
     for ingredient in ing_list:
         if ing_list.index(ingredient) == 0:
             return_str += ingredient
         else:
-            return_str += "%2" + ingredient
+            return_str += "%2C" + ingredient
     return return_str
 
 
@@ -45,6 +46,30 @@ def api_call(api_key, ing_list, num_results):
     data = response.json()
     return data
 
+
+def write_file(filename, dictionary):
+    """write dictionary to a json file"""
+    with open(filename, 'w') as open_file:
+        json.dump(dictionary, open_file)
+
+
+def api_call_test(api_key, ing_list, num_results):
+    """return a dictionary result based on an api call with dynamic
+    values for the ingredient list and number of results to display
+    """
+    stc_1 = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/"
+    stc_2 = "recipes/findByIngredients?fillIngredients=true&ingredients="
+    stc_3 = "&limitLicense=false&number="
+    stc_4 = "&ranking=1"
+    response = f"{stc_1}{stc_2}{ing_list}{stc_3}{num_results}{stc_4}"
+    print(response)
+
+
 if __name__ == "__main__":
-    # pprint(api_call(get_api_key(filename), test_ing_list, 2))
-    print(format_ingred_list(test_list))
+    ingredient_list = format_ingred_list(test_list)
+    api_key = get_api_key(filename)
+    response_to_write = api_call(api_key, ingredient_list, 2)
+    write_file('api_call.json', response_to_write)
+    # ingredient_list = format_ingred_list(test_list)
+    # api_key = get_api_key(filename)
+    # response_to_write = api_call_test(api_key, ingredient_list, 2)
