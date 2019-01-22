00c14bf804ef125aa30ca3180510c0482d098033
henrydaleywhite <henry.daley.white@gmail.com>
2019-01-22 14:59:49 -0500

    add base spoonacular api call code

---
+++ spoonacular_api.py
@@ -0,0 +1,44 @@
+import requests
+
+from pprint import pprint
+
+filename = "spoonacular_key.txt"
+test_ing_list = "apples%2Cflour%2Csugar"
+test_list = ['sugar', 'flour', 'apples']
+
+def get_api_key(filename):
+    """get the api key from a seperately stored file. 
+    Free API key for spoonacular api is available at 
+    https://market.mashape.com/spoonacular/recipe-food-nutrition/pricing
+    """
+    with open(filename, 'r') as open_file:
+        return open_file.read()
+
+
+def format_ingred_list(ing_list):
+    return_str = ""
+    for ingredient in ing_list:
+        if ing_list.index(ingredient) == 0:
+            return_str += ingredient
+        else:
+            return_str += "%2" + ingredient
+    return return_str
+
+
+def api_call(api_key, ing_list, num_results):
+    stc_1 = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/"
+    stc_2 = "recipes/findByIngredients?fillIngredients=true&ingredients="
+    stc_3 = "&limitLicense=false&number="
+    stc_4 = "&ranking=1"
+    response = requests.get(f"{stc_1}{stc_2}{ing_list}{stc_3}{num_results}{stc_4}",
+        headers={
+            "X-Mashape-Key": api_key,
+            "Accept": "application/json"
+        }
+    )
+    data = response.json()
+    return data
+
+if __name__ == "__main__":
+    # pprint(api_call(get_api_key(filename), test_ing_list, 2))
+    print(format_ingred_list(test_list))
